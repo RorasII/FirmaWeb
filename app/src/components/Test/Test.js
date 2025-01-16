d032715339a50a80e4652dee04604b819d8e608c
T4R4Sek <TAR4S@seznam.cz>
2025-01-16 22:02:29 +0100

    PRUDL :)

--- app/src/components/Test/Test.js
+++ app/src/components/Test/Test.js
@@ -1,16 +1,45 @@
 let prdel = 1;
+let prewTime = 0;
+let currTime = 0;
+let deltaTime = 0;
+/** @type {CanvasRenderingContext2D} */
+/** @type {HTMLCanvasElement} */
 
-export function drawSomething(ctx, time) {
+let x = 30;
+let y = 30;
+let size = 15;
+let vector = { x: 10, y: 10 };
+
+export function drawSomething(canvas, ctx, time) {
+  currTime = time;
+  deltaTime = (currTime - prewTime) * 0.001;
+  console.log(x);
   if (!ctx) return;
-  prdel += 1;
-  console.log(prdel);
-
-  // Clear the canvas
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-  // Draw something dynamic (e.g., based on time)
-  const x = (time / 10) % ctx.canvas.width; // Change position over time
-  const y = (time / 10) % ctx.canvas.height; // Change position over time
-  ctx.fillStyle = "blue";
-  ctx.fillRect(x, y, 50, 50); // Moving rectangle
+  console.log(`x: ${x}, y: ${y}, vector.x: ${vector.x}, vector.y: ${vector.y}`);
+  Move();
+
+  ctx.fillStyle = "black";
+  ctx.beginPath();
+  ctx.strokeStyle = "white";
+  ctx.arc(x, y, size, 0, 2 * Math.PI);
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+
+  function Move() {
+    x += vector.x * deltaTime;
+    y += vector.y * deltaTime;
+  }
+
+  // function Prudel(time) {
+  //   currTime = Math.floor((time / 1000) * 60);
+  //   if (prewTime != currTime) {
+  //     console.log(currTime);
+  //     if (Math.floor((time / 1000) * 60) % 60 == 0) console.log("prudel");
+  //   }
+  //   prewTime = currTime;
+  // }
+  prewTime = currTime;
 }
--- app/src/components/Test/Test.jsx
+++ app/src/components/Test/Test.jsx
@@ -8,12 +8,14 @@ function Test() {
   const canvasRef = useRef(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
+    /** @type {CanvasRenderingContext2D} */
+
+    const canvas = /** @type {HTMLCanvasElement} */ canvasRef.current;
     const ctx = canvas.getContext("2d");
     let animationFrameId;
 
     const render = (time) => {
-      drawSomething(ctx, time); // Call your draw function
+      drawSomething(canvas, ctx, time); // Call your draw function
       animationFrameId = requestAnimationFrame(render); // Request next frame
     };
 
@@ -26,7 +28,7 @@ function Test() {
 
   return (
     <div>
-      <canvas ref={canvasRef} width={1000} height={600} style={{ border: "1px solid black" }}></canvas>
+      <canvas ref={canvasRef} width={1920} height={800} style={{ border: "1px solid black" }}></canvas>
     </div>
   );
 }
